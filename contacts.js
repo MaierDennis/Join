a843dac708fba193ad31b740daddd5cf9cc85285
Dennis Maier <maier.dennis@outlook.de>
2022-12-13 13:52:34 +0100

    next

--- contacts.js
+++ contacts.js
@@ -2,19 +2,16 @@ function closeEditNewContact() {
     document.getElementById('add-contact').classList.add('d-none');
     document.getElementById('edit-contact').classList.add('d-none');
     document.getElementById('overlay').classList.add('d-none');
-    document.getElementById('close-div').classList.add('d-none');
 }
 
 function openNewContact() {
     document.getElementById('add-contact').classList.remove('d-none');
     document.getElementById('overlay').classList.remove('d-none');
-    document.getElementById('close-div').classList.remove('d-none');
 }
 
 function openEditContact(){
     document.getElementById('edit-contact').classList.remove('d-none');
     document.getElementById('overlay').classList.remove('d-none');
-    document.getElementById('close-div').classList.remove('d-none');
 }
 
 function showContacts() {
