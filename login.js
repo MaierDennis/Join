b5b2889d6ae34ecbf8123012ef7fab1d33cda1bf
nicmicdev <niclas.michel@gmail.com>
2022-12-20 17:00:14 +0100

    updates login process

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
 
-
 async function addUser() {
     let name = document.getElementById('name-signup');
     let email = document.getElementById('email-signup');
@@ -16,16 +15,14 @@ async function addUser() {
 function checkSignUp() {
     let email = document.getElementById('email-signup');
     let user = users.find(u => u.email === email.value);
-
-
     console.log(user);
-
     if (!user) {
         addUser();
     } else {
         document.getElementById('userExists').classList.remove('d-none');
         document.getElementById('goToLoginBtn').classList.remove('d-none');
         email.value = "";
+        email.focus();
         console.warn('User not found. Try again');
         return false
     }
@@ -34,10 +31,7 @@ function checkSignUp() {
 async function login() {
     let email = document.getElementById('email-login');
     let password = document.getElementById('password-login');
-
-    let user = users.find(u => u.email === email.value && u.password == password.value);
-    console.log(user);
-
+    let user = users.find(u => u.email === email.value && u.password == password.value)
     if (user) {
         await localStorage.setItem('activeUser', JSON.stringify(user.name));  // saving active user in database 
         if (document.body.clientWidth > 1024) {
@@ -51,15 +45,10 @@ async function login() {
         email.value = "";
         password.value = "";
     }
-
-
 }
 
 async function guestLogin() {
-    let guest = {
-        'name': 'Guest',
-    }
-
+    let guest = {'name': 'Guest',};
     await localStorage.setItem('activeUser', JSON.stringify(guest.name));  // saving guest as activeuser in backend
     if (document.body.clientWidth > 1024) {
         window.location.href = 'summary.html';
@@ -69,22 +58,15 @@ async function guestLogin() {
 }
 
 function checkIfUserExists(e) {
-
     let email = document.getElementById('forgot-pw-mail');
     let user = users.find(u => u.email === email.value);
-
-    console.log(user);
-
     if (user) {
         return true
-        // console.log('User exists:', user);
-        // email.value = '';
     } else {
         e.preventDefault();
         document.getElementById('forgot-user-not-found').classList.remove('d-none');
         email.value = "";
         console.warn('User not found. Try again');
-
         return false
     }
 
@@ -93,31 +75,23 @@ function checkIfUserExists(e) {
 async function changePassword() {
     const urlParams = new URLSearchParams(window.location.search);
     const email = urlParams.get('email');
-
     let newPassword = document.getElementById('new-password');
     let newPasswordConfirm = document.getElementById('new-password-confirm');
-
     let findMailInUsers = users.find(u => u.email == email);
-
     if (newPassword.value == newPasswordConfirm.value) {
         findMailInUsers.password = newPassword.value;
-
         console.log('Changing password for this user: ', email);
         console.log('Password changed to:', findMailInUsers.password);
-
         await backend.setItem('users', JSON.stringify(users));
         window.location.href = 'success_reset_pw.html'
         newPassword.value = '';
         newPasswordConfirm.value = '';
-
     } else {
         document.getElementById('user-not-found').classList.remove('d-none');
         newPassword.value = '';
         newPasswordConfirm.value = '';
     }
 }
-
-
 
 function goToLogin() {
     window.location.href = 'index.html';
@@ -138,6 +112,7 @@ function changePwToText(id, name) {
     document.getElementById(`pw-no-show-${name}`).classList.add('d-none');
     document.getElementById(`pw-show-${name}`).classList.remove('d-none');
     document.getElementById(id).type = 'text';
+    
 }
 
 
@@ -147,38 +122,8 @@ function changeTextToPw(id, name) {
     document.getElementById(id).type = 'password';
 }
 
-function changePwIcon(id, name) {
-
-    const typeIsPassword = document.getElementById(id).type == 'password';
-
-    if (typeIsPassword) {
-        changePwIconToEye(name);
-    } else {
-        
-
-    }
-
-
-
-
+function checkInputType(id, name) {
+    let typeIsPassword = document.getElementById(id).type == 'password';
+    if (typeIsPassword == true) changePwIconToEye(name);
 }
-
-
-
-// function changePwIconToLock() {
-//     document.getElementById('pw-icon-no-show').classList.add('d-none');
-//     document.getElementById('pw-icon').classList.remove('d-none');
-
-// }
-
-
-
-
-// function getData() {
-//     backend.getItem('users');
-//     JSON.parse(backend.getItem('users'));
-//     users = JSON.parse(backend.getItem('users'));
-//     console.log(users);
-
-// }
 
